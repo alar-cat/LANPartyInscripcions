bd8e392407fce0fe620e6e9b8f8a1e21e4e809a3
Aniol Martí <aniol.me@gmail.com>
2017-01-22 01:31:27 +0100

    Added QR.

--- inscripcions/includes/qr.php
+++ inscripcions/includes/qr.php
@@ -1,9 +1,26 @@
 <?php
+include "libs/phpqrcode/phpqrcode.php";
 
-function generateQR() {
+function generateQR($nom,$cognoms,$dni,$nick,$naixement) {
+  // Es comprova si és major d'edat.
+  $major = "No";
+  $esdeveniment = "05-11-2017"; //Data de la festa per calcular si és major d'edat.
+  $naixement = str_replace('/', '-', $naixement);
+  $temps = strtotime($naixement);
+  if ($temps < (strtotime($esdeveniment) - (18 * 60 * 60 * 24 * 365.25))) {
+    $major = "Sí";
+  }
+  $informacio = "Nom: $nom $cognoms\n";
+  $informacio .= "DNI: $dni\n";
+  $informacio .= "Nick: $nick\n";
+  $informacio .= "Major: $major\n";
+  $clau = "password";
+  $ruta = "."; //Ruta per guardar els QRs.
 
+  $iv = mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC),MCRYPT_DEV_URANDOM);
+  $encrypted = base64_encode($iv . mcrypt_encrypt(MCRYPT_RIJNDAEL_128,hash('sha256', $clau, true),$informacio,MCRYPT_MODE_CBC,$iv));
 
-
+  QRcode::png($encrypted, "$ruta/$dni.png", 'L', 10, 2); // creates file
 
 }
 
--- inscripcions/input.php
+++ inscripcions/input.php
@@ -3,12 +3,13 @@
     <meta charset="UTF-8">
     <title>Inscripció LAN Party Ripoll</title>
     <h1>Inscripció LAN Party Ripoll</h1>
-    <meta http-equiv="refresh" content="10; url=https://www.lanpartyripoll.cat" />
+    <!--<meta http-equiv="refresh" content="10; url=https://www.lanpartyripoll.cat" />-->
   </head>
 
 <?php
 include "includes/conn.php";
 include "includes/send.php";
+include "includes/qr.php";
 
 // Agafo les dades del formulari.
 $nom = htmlspecialchars($_POST["nom"]);
@@ -35,12 +36,14 @@ elseif ($categoria == "FIFA16") {
 $nick = htmlspecialchars($_POST["nick"]);
 $dni = htmlspecialchars($_POST["dni"]);
 $naixement = htmlspecialchars($_POST["dianaixement"]) . "/" . htmlspecialchars($_POST["mesnaixement"]) . "/" . htmlspecialchars($_POST["anynaixement"]);
+$naixement = str_replace(' ', '', $naixement);
 $poblacio = htmlspecialchars($_POST["poblacio"]);
 
 // Search DB
 $busca = mysqli_query($conn, "SELECT id FROM inscripcions WHERE dni = '$dni'");
 if($busca->num_rows == 0) {
      $order = "INSERT INTO inscripcions (nom,cognom,email,telefon,categoria,equip,nick,dni,poblacio,naixement) VALUES ('$nom','$cognoms','$email','$telefon','$categoria','$equip','$nick','$dni','$poblacio','$naixement')";
+     generateQR($nom,$cognoms,$dni,$nick,$naixement);
 }
 else {
     $resultat = mysqli_query($conn, "SELECT equip FROM inscripcions WHERE dni = '$dni'");
@@ -61,7 +64,7 @@ else {
 // Si es poden insertar les dades a la base de dades s'envia un correu de confirmació a l'administrador i a l'inscrit.
 if (mysqli_query($conn, $order)) {
   envia($email);
-	echo "<p>T'has inscrit correctament, gràcies! T'hem enviat un correu-e amb més informació.<p>";
+	echo "<p>T'has inscrit correctament, gràcies!<p>";
 }
 
 else {
