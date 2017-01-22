4732b273fb8a56c83545cb495f0e6bd8ff77c921
Aniol Martí <aniol.me@gmail.com>
2017-01-22 17:04:32 +0100

    Updated encryption.

--- inscripcions/includes/qr.php
+++ inscripcions/includes/qr.php
@@ -16,10 +16,8 @@ function generateQR($nom,$cognoms,$dni,$nick,$naixement) {
   $informacio .= "Major: $major\n";
   $clau = "password";
   $ruta = "/var/www/lanpartyQR"; //Ruta per guardar els QRs.
-
-  $iv = mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC),MCRYPT_DEV_URANDOM);
-  $encrypted = base64_encode($iv . mcrypt_encrypt(MCRYPT_RIJNDAEL_128,hash('sha256', $clau, true),$informacio,MCRYPT_MODE_CBC,$iv));
-
+  $encrypted = shell_exec('echo -n "' . $informacio . '" | openssl enc -aes-256-cbc -a -k ' . $clau);
+  echo $encrypted;
   QRcode::png($encrypted, "$ruta/$dni.png", 'L', 10, 2); // creates file
 
 }
